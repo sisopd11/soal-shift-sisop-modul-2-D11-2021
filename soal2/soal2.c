31462a28a729e75770a253cb642a52f2dc2723a9
sisopd11 <80894892+sisopd11@users.noreply.github.com>
2021-04-17 19:18:17 +0800

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -3,24 +3,21 @@
 #include<unistd.h>
 #include<fcntl.h>
 #include<sys/types.h>
-#include<sys/wait.h>
+#include<wait.h>
+#include<string.h>
+#include<dirent.h>
 
-int status;
+void soal2b(char *basePath);
 void soal2a();
-void soal2b();
 
 int main()
 {
-  soal2a();
-  soal2b();
-  soal2c();
-  soal2d();
-  soal2e();
-  return 0;
+   soal2a();
 }
 
 void soal2a()
 {
+  int status;
   pid_t child_id;
   child_id=fork();
 
@@ -29,14 +26,60 @@ void soal2a()
   }
 
   if(child_id==0){
-    char *argv[]={"mkdir","-p","/home/dewi/modul2/petshop",NULL};
-    execv("/usr/bin/mkdir",argv);
+      char *argv[]={"unzip","/home/dewi/modul2/pets.zip","-d","/home/dewi/modul2/petshop","*.jpg",NULL};
+      execv("/usr/bin/unzip",argv);
   }
-  
-  else{
 
-    //*.jpg agar yg terunzip hny file jpg
-    char *argv[]={"unzip","/home/dewi/modul2/pets.zip","-d","/home/dewi/modul2/petshop","*.jpg",NULL};
-    execv("/usr/bin/unzip",argv);
+  else
+  {
+     while(wait(&status)>0);
+     soal2b("petshop");
   }
+
+}
+
+//traverse dir scr rekursif
+void soal2b(char *basePath)
+{
+  DIR *dir;
+  //menampung variabelnya
+  struct dirent *dp;
+  char path[1000];
+  char *str;
+  char nama[100];
+  char *temp;
+
+  dir = opendir(basePath);
+
+  if(!dir) return;
+
+  while((dp=readdir(dir))!=NULL){
+     int i=0;
+
+     if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0){
+     str = dp->d_name;
+     snprintf(nama,sizeof nama, "%s", dp->d_name);
+     temp=strtok(str,";_");
+     
+     while(temp != 0){
+        if(i==0||i==3){
+	     if(fork()==0){
+                 char buf[100];
+                 snprintf(buf,sizeof buf, "/home/dewi/modul2/petshop/%s", temp);
+                 char *argv[]={"mkdir","-p","/home/dewi/modul2/petshop",buf,NULL};
+                 execv("/bin/mkdir",argv);
+		//printf("%s\n",buf);
+            }
+        }
+        temp = strtok(NULL,";_");
+        i++;
+     }
+     strcpy(path,basePath);
+     strcat(path, "/");
+     strcat(path, dp->d_name);
+    
+     soal2b(path);
+  }    	 
+ } 
+   closedir(dir); 
 }
